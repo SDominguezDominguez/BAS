f000937328ae8e8ae82471ff505c2e86b404a74e
SDominguezDominguez <sdominguezdominguez@hotmail.com>
2023-03-09 16:27:56 +0100

    created repositories

---
+++ src/main/java/com/example/BAS/repositories/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.example.BAS.repositories;
+
+import com.example.BAS.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
