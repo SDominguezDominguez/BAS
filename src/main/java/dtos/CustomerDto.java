42c634371ae7185b58851d29d70cd7a71e50f4fe
SDominguezDominguez <sdominguezdominguez@hotmail.com>
2023-03-29 19:56:19 +0200

    Customer dto en input dto gemaakt

---
+++ src/main/java/dtos/CustomerDto.java
@@ -0,0 +1,52 @@
+package dtos;
+
+import com.example.BAS.enumerations.Label;
+
+public class CustomerDto {
+
+    private Long id;
+    private String name;
+    private String customerNumber;
+    private Label label;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public void setLabel(Label label) {
+        this.label = label;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/dtos/CustomerInputDto.java
@@ -0,0 +1,52 @@
+package dtos;
+
+import com.example.BAS.enumerations.Label;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public class CustomerInputDto {
+
+    @NotEmpty(message = "Naam is verplicht")
+    private String name;
+
+    @NotEmpty(message = "Klantnummer is verplicht")
+    private String customerNumber;
+
+    @NotEmpty(message = "Merk is verplicht")
+    private Label label;
+
+    @Email
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public void setLabel(Label label) {
+        this.label = label;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
