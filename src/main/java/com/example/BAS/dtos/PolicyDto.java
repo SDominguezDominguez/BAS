6b35e79c8877b230410293647fd934931432b052
SDominguezDominguez <sdominguezdominguez@hotmail.com>
2023-03-30 19:43:16 +0200

    created Policy dto's

---
+++ src/main/java/com/example/BAS/dtos/PolicyDto.java
@@ -0,0 +1,61 @@
+package com.example.BAS.dtos;
+
+import java.time.LocalDate;
+
+public class PolicyDto {
+
+    private Long id;
+    private String policyNumber;
+    private Integer amount;
+    private LocalDate receiveDateAmount;
+    private LocalDate receiveDatePsk;
+    private LocalDate reminderDatePsk;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getReceiveDateAmount() {
+        return receiveDateAmount;
+    }
+
+    public void setReceiveDateAmount(LocalDate receiveDateAmount) {
+        this.receiveDateAmount = receiveDateAmount;
+    }
+
+    public LocalDate getReceiveDatePsk() {
+        return receiveDatePsk;
+    }
+
+    public void setReceiveDatePsk(LocalDate receiveDatePsk) {
+        this.receiveDatePsk = receiveDatePsk;
+    }
+
+    public LocalDate getReminderDatePsk() {
+        return reminderDatePsk;
+    }
+
+    public void setReminderDatePsk(LocalDate reminderDatePsk) {
+        this.reminderDatePsk = reminderDatePsk;
+    }
+}
