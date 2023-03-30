9a96b1fa0464b00af3f7c6654e4448d2f909e19a
SDominguezDominguez <sdominguezdominguez@hotmail.com>
2023-03-30 20:59:24 +0200

    created Company dto's

---
+++ src/main/java/com/example/BAS/dtos/CompanyInputDto.java
@@ -0,0 +1,39 @@
+package com.example.BAS.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public class CompanyInputDto {
+
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String contactPerson;
+    @NotEmpty
+    @Email
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
