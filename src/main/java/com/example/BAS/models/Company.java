2ca7d8fe13c35cf8212316ca40153916b7a1fe22
SDominguezDominguez <sdominguezdominguez@hotmail.com>
2023-03-30 20:57:19 +0200

    created Company entity

---
+++ src/main/java/com/example/BAS/models/Company.java
@@ -0,0 +1,59 @@
+package com.example.BAS.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(generator = "company_sequence")
+    @GenericGenerator(
+            name = "company_sequence",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "company_sequence", value = "customer_id"),
+                    @Parameter(name = "initial_value", value = "4"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private Long id;
+    private String name;
+    private String contactPerson;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
