a9aece7d2d98a3d6d74a40d5bd1d367deecbcfc7
SDominguezDominguez <sdominguezdominguez@hotmail.com>
2023-03-30 20:55:01 +0200

    created getFileByPolicyNumber method and added relations in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ VALUES (1, 'WACHTEN_OP_PSK_EN_BEDRAG', 'Nabestaandendossier', 'Lhk nee', 'LUR', 
        (2, 'BIJZONDERHEID', 'Niet compliant', 'Dep ja', 'LOR', 0, 2),
        (3, 'WACHTEN_OP_BEDRAG', '', 'Lhk ja', 'GHUR', 4000, 3);
 
-INSERT INTO policy(id, policy_number, amount, receive_date_amount, receive_date_psk, reminder_date_psk)
-VALUES (1, 'L123', 2500, '2023-02-01', '2023-01-01', null),
-       (2, 'L1234', null, null, null, null),
-       (3, 'K504', 6593.21, '2023-03-10', null, '2023-03-24');
+INSERT INTO policy(id, policy_number, amount, receive_date_amount, receive_date_psk, reminder_date_psk, file_id)
+VALUES (1, 'L123', 2500, '2023-02-01', '2023-01-01', null, 1),
+       (2, 'L1234', null, null, null, null, 2),
+       (3, 'K504', 6593.21, '2023-03-10', null, '2023-03-24', 3);
